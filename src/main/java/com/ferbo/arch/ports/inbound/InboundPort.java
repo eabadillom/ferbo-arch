fb5ddeb76489bd87c5038aa2cf54f624ee0afe34
julio <julio@juliopc.ferbo>
2026-03-26 14:01:56 -0600

    feat: agregar package ports (inbound/outbound) para arquitectura hexagonal

---
+++ src/main/java/com/ferbo/arch/ports/inbound/InboundPort.java
@@ -0,0 +1,8 @@
+package com.ferbo.arch.ports.inbound;
+
+/**
+ * Contrato base para inbound adapters.
+ * Sirve como referencia y marcador de paquetes para la arquitectura hexagonal.
+ */
+public interface InboundPort {
+}
---
+++ src/main/java/com/ferbo/arch/ports/outbound/OutboundPort.java
@@ -0,0 +1,7 @@
+package com.ferbo.arch.ports.outbound;
+
+/**
+ * Contrato base para outbound adapters.
+ */
+public interface OutboundPort {
+}
